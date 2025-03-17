be0baf0cbe18a670e16b27d6f954a74e22af93c2
Maros Masurik <masurik1@gmail.com>
2025-03-17 10:02:47 +0100

    LIKENEJDE

--- src/app/actions/createLike.ts
+++ src/app/actions/createLike.ts
@@ -2,15 +2,18 @@ import { prisma } from "@/app/api/auth/[...nextauth]/prisma";
 
 export const createLike = async (postId: string, userId: string) => {
   try {
+    console.log("Creating like for postId:", postId, "and userId:", userId);
+
     const like = await prisma.like.create({
       data: {
         postId,
         userId,
       },
     });
+
     return like;
-  } catch (error) {
-    console.error('Error creating like:', error);
-    throw new Error('Failed to create like');
+  } catch (error: any) {
+    console.error("Error creating like:", error?.message || error);
+    throw new Error(error?.message || "Failed to create like");
   }
 };
